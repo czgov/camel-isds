aef81506a967f8527eccd1c69f1b6a3335b962dc
Josef Ludvicek <josef.ludvicek.cz@gmail.com>
2017-05-10 17:41:09 +0200

    [relates #31] Use camel case instead of "." in camel header names

--- src/main/java/com/github/czgov/isds/Constants.java
+++ src/main/java/com/github/czgov/isds/Constants.java
@@ -4,12 +4,12 @@ package com.github.czgov.isds;
  * Created by jludvice on 9/22/16.
  */
 public class Constants {
-    public static final String PREFIX = "isds.";
+    public static final String PREFIX = "isds";
 
-    public static final String MSG_SUBJECT = PREFIX + "subject";
-    public static final String MSG_ID = PREFIX + "id";
-    public static final String MSG_FROM = PREFIX + "from";
-    public static final String MSG_TO = PREFIX + "to";
+    public static final String MSG_SUBJECT = PREFIX + "Subject";
+    public static final String MSG_ID = PREFIX + "Id";
+    public static final String MSG_FROM = PREFIX + "From";
+    public static final String MSG_TO = PREFIX + "To";
 
     public static final String DEFAULT_ATTACHMENT_STORE = "camel-isds-attachment-store";
 }
